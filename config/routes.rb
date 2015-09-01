aabaffee8b660aee197a57439eb7a01c33614c52
giuseppe de santis <giusdesan@gmail.com>
2015-09-01 11:56:30 +0100

    set up restaurants resources

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   #     resource :seller
   #   end
 
+  resources :restaurants
+
   # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
