94a6282932a72814bf575d1f1d2996abb1cd5f12
Leon Wee <leonwee27@gmail.com>
2015-09-04 13:41:13 +0700

    Extended to a better controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,11 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, -> { extending WithUserAssociationExtension }, dependent: :destroy
+
   validates :name, length: { minimum: 3 }, uniqueness: true
   validates_presence_of :user
   belongs_to :user
 
-  def build_review(params, user)
-    self.reviews.new(thoughts: params[:thoughts], rating: params[:rating], user_id: user.id)
-
-  end
+  # def build_review(params, user)
+  #   self.reviews.new(thoughts: params[:thoughts], rating: params[:rating], user_id: user.id)
+  # end
 end
