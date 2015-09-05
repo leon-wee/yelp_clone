698f546f1e65131e772a2630d336f6fdfa73c37d
leon-wee <leonwee27@gmail.com>
2015-09-05 12:19:08 +0700

    Passed endorsements but need to put more

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   include AsUserAssociationExtension
-
+  has_many :endorsements
   belongs_to :restaurant
   belongs_to :user
   validates :rating, inclusion: (1..5)
